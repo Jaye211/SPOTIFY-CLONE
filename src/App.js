d9a00930edd8d1382edc8c7cc27a7f48269694ca
jaye211 <jaye2049@gmail>
2023-06-14 10:50:39 -0600

    yes commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react'
+import  "./App.css";
+import Login from './Login';
+import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from "spotify-web-api-js";
+import Player from './Player';
+import { useDataLayerValue } from './DataLayer';
+
+//09035ff74fd44b688aa61aff28d6e53a
+
+const spotify = new SpotifyWebApi();
 
 function App() {
+const [{ user, token }, dispatch] = useDataLayerValue();
+
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash ="";
+    const _token = hash.access_token;
+
+    if (_token) {
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token
+      })
+
+      spotify.setAccessToken(_token);
+      spotify.getMe().then((user) => {
+        
+
+        dispatch({
+          type: 'SET_USER',
+          user: user,
+        });
+      });
+
+      spotify.getUserPlaylists().then((playlists) => {
+        dispatch({
+          type: "SET_PLAYLISTS",
+          playlists: playlists,
+        });
+      });
+      spotify.getPlaylist("37i9dQZEVXcIJazRV9ISoM").then(response =>
+        dispatch({
+          type: "SET_DISCOVER_WEEKLY",
+          discover_weekly: response,
+        })
+        );
+    }
+
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      {
+        token ? (
+          <Player />
+        ) : (
+          <Login />
+        )
+      }
+      
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
